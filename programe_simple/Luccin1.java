0f1fde209fcad4c7e0f49300cb22c7871a657a07
unknown <cecce@FRCLOC203-67412.ecolecatholique.ca>
2020-11-17 09:13:22 -0500

    petite exercice

--- programe_simple/Luccin1.java
+++ programe_simple/Luccin1.java
@@ -1,14 +1,6 @@
-//coucou
 
-public class Luccin1 // la classe principale ayant le même nom 
-                     // que le fichier
-{
-	public static void main(String[] args) // la méthode main
-	                                       // script du programme
-	{
-		System.out.println("Hello World"); // une déclaration à exécuter,
-		                                   // terminée avec;
-	} // les blocs de code sont enfermés par des accolades {}
-
-} // les méthodes (comme main) et les classes sont deux types de blocs de code comme 
-  // les blocs de conditions( if... else) et de boucles qu'on connais déjà
+public class Luccin1{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
