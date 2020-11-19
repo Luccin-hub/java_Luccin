b7b585d2dbb7f22323430ca3bc7a5aa2617a5f3d
unknown <cecce@FRCLOC203-67412.ecolecatholique.ca>
2020-11-19 14:40:42 -0500

    Petite ajustement

--- programe_simple/base_Luccin.java
+++ programe_simple/base_Luccin.java
@@ -1,23 +1,56 @@
+/*  Par: Luccin Pierre Mutabazi, mutluc18@ecolecatholique.ca 
+    2020-11-18
+*/
+
+// Au début du fichier, avant la déclaration de classe...
+// importer la définition de l'objet Scanner 
 import java.util.Scanner;
+/** Exemples sur les types et les entrées et sorties au console */
 public class base_Luccin {
     public static void main(String[] args) 
     {
-        String s = "45";
-        int n = 3;
-        double d = 3.14;
+        //----------TYPES DE DONNÉES----------//
 
+        // Il faut déclarer le TYPE des variables
+        // Format : <type> <nom> = <valeur assignée>;
+
+        String s = "100";// texte -> usage général
+        int n = 3;// nombres entiers -> pour compter
+        double d = 3.14;// nombres décimaux -> pour les calculs
+
+        // conversions de type (texte -> nombre)
         n = Integer.parseInt(s);
         d = Double.parseDouble(s);
-
-
+        // casting entre types numériques
         n = (int) d;
         d = n;
+//-----------SORTIE À LA CONSOLE-----------//
+        
+        // System.out 
+        // est la console définit par ton système d'exploitation
 
+        // .println() ajoute automatiquement un retour de ligne
         System.out.println(s);
-        System.out.print(s);
+        System.out.println(s);
+         //-----------ENTRÉES À LA CONSOLE------------//
+
+        // Tu dois créer un objet de type Scanner
+
+        // System.in 
+        // est la console définit par ton système d'exploitation
         Scanner input = new Scanner(System.in);
-        System.out.println("taper un nombre decimal");
+        // Indiquer à l'utilisateur d'entrer quelque chose
+        System.out.println("taper un nombre décimal");
+        // Saisir le texte entré avec la méthode .nextLine()
         String getnum = input.nextLine();
-        double num = Double.parseDouble(getnum); 
+        // Convertir le texte capté en valeur numérique (de type double)
+        // --> cette étape peut générer une erreur si le texte n'a pas 
+        //     le format approprié
+        double num = Double.parseDouble(getnum);
+        // Confirmer le résultat avec un message à la console
+        System.out.println("Tu as inscrit le nombre " + num);
+        // À la fin de la méthode main()...
+        // fermer/tuer le Scanner qui observe la console / System.in
+        input.close(); 
     }
 }
