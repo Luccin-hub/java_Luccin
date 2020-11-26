1ba3d7f11ff3158f8fd5922fe2917170b667d6ee
unknown <cecce@FRCLOC203-67412.ecolecatholique.ca>
2020-11-26 14:51:00 -0500

    projet sommative

---
+++ formatif_1/projet.java
@@ -0,0 +1,6 @@
+public class projet {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans UNE QUESTION POUR UN CHAMPION");
+        System.out.println("Choisir votre Section");
+    }
+}
