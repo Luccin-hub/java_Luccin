2045de32a29c2f869bb299ffc4fee145806068ed
unknown <cecce@FRCLOC203-67412.ecolecatholique.ca>
2020-11-27 11:51:49 -0500

    Nouveau projet

--- formatif_1/projet.java
+++ formatif_1/projet.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 public class projet {
     public static void main(String[] args) {
-        System.out.println("Bienvenue dans UNE QUESTION POUR UN CHAMPION");
-        System.out.println("Choisir votre Section");
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("BIENVENUE");
+        System.out.println();
+        System.out.println("pour une blague taper 1");
+        System.out.println("pour une devinette taper 2");
+        System.out.println("pour quitter taper 3");
+        String reponse = input.nextLine();
+        
+        
+         int n = Integer.parseInt(reponse);
+
+       
+        
+        if (n==1)
+        {System.out.println("Pourquoi toute les fermes sont silencieuse? parce qu'il LA FERME");}
+        else if (n==2)
+        {System.out.println("Qu'est ce qui a 13 coeurs mais aucun organe? UN JEU DE CARTE");}
+        else 
+        {System.out.println("Au revoir");}
+        input.close(); 
     }
+
 }
